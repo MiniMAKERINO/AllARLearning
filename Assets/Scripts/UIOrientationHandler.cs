0a28ed30330f05c3dd3d13a076e57e125581a794
MiniMAKERINO <jd_mak@yahoo.com.au>
2019-06-10 17:15:37 +1000

    Incomplete build v1
    - Functioning axis detection modules
    - Functioning model display
    - Functioning LanscapeLeftCanvas UI

---
+++ Assets/Scripts/UIOrientationHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOrientationHandler : MonoBehaviour
+{
+
+    public GameObject PortraitCanvas;
+    public GameObject LandscapeLeftCanvas;
+    public GameObject LandscapeRightCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.orientation == ScreenOrientation.Portrait) {
+            PortraitCanvas.SetActive(true);
+
+            LandscapeLeftCanvas.SetActive(false);
+            LandscapeRightCanvas.SetActive(false);
+        }
+
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft)
+        {
+            LandscapeLeftCanvas.SetActive(true);
+
+            PortraitCanvas.SetActive(false);
+            LandscapeRightCanvas.SetActive(false);
+        }
+
+        if (Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            LandscapeRightCanvas.SetActive(true);
+
+            PortraitCanvas.SetActive(false);
+            LandscapeLeftCanvas.SetActive(false);
+        }
+    }
+}
