f7f5a2a95aeb7aa24bb10f17da2f81cefe4782e8
MiniMAKERINO <jd_mak@yahoo.com.au>
2019-06-10 20:32:20 +1000

    Added mute and unmute icon function

---
+++ Assets/Scripts/AudioCtrl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCtrl : MonoBehaviour
+{
+    public GameObject PortraitUnmutedIcon;
+    public GameObject PortraitMutedIcon;
+
+    public GameObject LandscapeLeftUnmutedIcon;
+    public GameObject LandscapeLeftMutedIcon;
+
+    public GameObject LandscapeRightUnmutedIcon;
+    public GameObject LandscapeRightMutedIcon;
+
+    void Start()
+    {
+
+       // Obsolete Code below commented out:
+       // PortraitUnmutedIcon.SetActive(true);
+       // PortraitMutedIcon.SetActive(false);
+       
+       // LandscapeLeftUnmutedIcon.SetActive(true);
+       // LandscapeLeftMutedIcon.SetActive(false);
+
+       // LandscapeRightUnmutedIcon.SetActive(true);
+       // LandscapeRightMutedIcon.SetActive(false);
+    }
+
+    public void ToggleAudio() //Toggle audio and mute/unmute icon function
+    {
+        // Get state of the unmute and mute icons for Portrait canvas as a bool, then switches the 'active-state' of the icons
+        // to display one at a time, and let the user know that SFX is either ON or OFF.
+        bool PortraitUnmutedIcon_CurrentState = PortraitUnmutedIcon.activeSelf;
+        bool PortraitMutedIcon_CurrentState = PortraitMutedIcon.activeSelf;
+            PortraitUnmutedIcon.SetActive(!PortraitUnmutedIcon_CurrentState);
+            PortraitMutedIcon.SetActive(!PortraitMutedIcon_CurrentState);
+
+        bool LandscapeLeftUnmutedIcon_CurrentState = LandscapeLeftUnmutedIcon.activeSelf;
+        bool LandscapeLeftMutedIcon_CurrentState = LandscapeLeftMutedIcon.activeSelf;
+            LandscapeLeftUnmutedIcon.SetActive(!LandscapeLeftUnmutedIcon_CurrentState);
+            LandscapeLeftMutedIcon.SetActive(!LandscapeLeftMutedIcon_CurrentState);
+
+        bool LandscapeRightUnmutedIcon_CurrentState = LandscapeRightUnmutedIcon.activeSelf;
+        bool LandscapeRightMutedIcon_CurrentState = LandscapeRightMutedIcon.activeSelf;
+            LandscapeRightUnmutedIcon.SetActive(!LandscapeRightUnmutedIcon_CurrentState);
+            LandscapeRightMutedIcon.SetActive(!LandscapeRightMutedIcon_CurrentState);
+    }
+}
