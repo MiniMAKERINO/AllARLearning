bdc4982c1f82fa66aa48e449f4553abb73c41512
MiniMAKERINO <jd_mak@yahoo.com.au>
2019-06-11 16:21:23 +1000

    Mute Button Works

--- Assets/Scripts/AudioCtrl.cs
+++ Assets/Scripts/AudioCtrl.cs
@@ -13,22 +13,27 @@ public class AudioCtrl : MonoBehaviour
     public GameObject LandscapeRightUnmutedIcon;
     public GameObject LandscapeRightMutedIcon;
 
+    public GameObject AudioListenerObject;
+
     void Start()
     {
+    // Obsolete Code below commented out:
+    // PortraitUnmutedIcon.SetActive(true);
+    // PortraitMutedIcon.SetActive(false);
 
-       // Obsolete Code below commented out:
-       // PortraitUnmutedIcon.SetActive(true);
-       // PortraitMutedIcon.SetActive(false);
-       
-       // LandscapeLeftUnmutedIcon.SetActive(true);
-       // LandscapeLeftMutedIcon.SetActive(false);
+    // LandscapeLeftUnmutedIcon.SetActive(true);
+    // LandscapeLeftMutedIcon.SetActive(false);
 
-       // LandscapeRightUnmutedIcon.SetActive(true);
-       // LandscapeRightMutedIcon.SetActive(false);
-    }
+    // LandscapeRightUnmutedIcon.SetActive(true);
+    // LandscapeRightMutedIcon.SetActive(false);
+}
 
     public void ToggleAudio() //Toggle audio and mute/unmute icon function
     {
+
+        bool AudioListenerObject_CurrentState = AudioListenerObject.activeSelf;
+        AudioListenerObject.SetActive(!AudioListenerObject_CurrentState);
+
         // Get state of the unmute and mute icons for Portrait canvas as a bool, then switches the 'active-state' of the icons
         // to display one at a time, and let the user know that SFX is either ON or OFF.
         bool PortraitUnmutedIcon_CurrentState = PortraitUnmutedIcon.activeSelf;
